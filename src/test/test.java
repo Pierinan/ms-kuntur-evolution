fc52b7dc91315dfe7490549a50f66306f6d44337
Pierina <pierinan@outlook.com>
2024-08-14 16:06:45 -0500

    Test test

--- src/test/test.java
+++ src/test/test.java
@@ -1,15 +1,5 @@
 public class Test { //SAFE CHANGES
 
-    public static void main(String[] args) {
-        // Ejemplo simple de una prueba unitaria básica
-        int result = add(2, 3);
-        if (result == 5) {
-            System.out.println("Test passed.");
-        } else {
-            System.out.println("Test failed.");
-        }
-    }
-
     // Método simple que realiza una suma
     public static int add(int a, int b) {
         return a + b;
